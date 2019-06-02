0f6428c15fc02e4d3145a13f97308c5c84862514
SaeedBoroo <saeed.boroomand508@gmail.com>
2019-06-02 16:01:29 +0430

    Add ngrx devTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,28 @@ import { AppComponent } from './app.component';
 import { MyCounterComponent } from './counter/my-counter/my-counter.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer }  from './counter/my-counter.reducer'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    StoreModule.forRoot({ count: counterReducer })
+    StoreModule.forRoot({ count: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: false,
+      features: {
+        pause: true, // start/pause recording of dispatched actions
+        lock: true, // lock/unlock dispatching actions and side effects    
+        persist: true, // persist states on page reloading
+        export: true, // export history of actions in a file
+        import: 'custom', // import history of actions from a file
+        jump: true, // jump back and forth (time travelling)
+        skip: true, // skip (cancel) actions
+        reorder: true, // drag and drop actions in the history list 
+        dispatch: true, // dispatch custom actions or action creators
+        test: true // generate tests for the selected actions
+      },
+    }),
      ],
   declarations: [ AppComponent, MyCounterComponent ],
   bootstrap:    [ AppComponent ]
